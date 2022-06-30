bbed9210eba063a24b4fb4ebc8bee5b8832013b3
Frans Vergeest <26468902+frans32@users.noreply.github.com>
2022-06-30 21:38:27 +0200

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -18,7 +18,7 @@ export default function Header(props) {
         </a>
       </Link>
       <div className={styles.uitgawe}>
-        Mei 2022 <span className={styles.desktopOnly}>/ Jaargang 43</span>
+        Junie 2022 <span className={styles.desktopOnly}>/ Jaargang 43</span>
       </div>
     </header>
   );
