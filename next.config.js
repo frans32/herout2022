6df34858b485b113324153b5df83e47c1a402ad2
Frans Vergeest <frans@Bubble.local>
2022-07-02 23:26:01 +0200

    Updated and switched to Preact

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer && false) {
+    if (!dev && !isServer) {
       Object.assign(config.resolve.alias, {
         react: "preact/compat",
         "react-dom/test-utils": "preact/test-utils",
