f8d0984842fa945cbad2d0f2f1ea77a7dffdf89b
Frans Vergeest <frans@Bubble.local>
2022-09-04 14:18:35 +0200

    Logo strutured data

--- pages/_app.js
+++ pages/_app.js
@@ -22,6 +22,15 @@ function MyApp({ Component, pageProps }) {
           crossOrigin="true"
         />
 
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            url: "https://herout.co.za",
+            logo: "https://herout.co.za/favicon/logo256.png",
+          })}
+        </script>
+
         <script defer src="https://herout.co.za/plausible.js"></script>
       </Head>
       <Component {...pageProps} />
